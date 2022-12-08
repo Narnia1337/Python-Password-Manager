70df2f652413908ebd97b539ef535ba24266dca6
Narnia1337 <102632186+Narnia1337@users.noreply.github.com>
2022-12-08 15:18:28 -0600

    Add files via upload

---
+++ password manager.py
@@ -0,0 +1,29 @@
+import random
+import string
+
+# create a function to generate a random password
+def generate_password():
+    # generate a random string of lowercase and uppercase letters and digits
+    password = "".join(random.choices(string.ascii_letters + string.digits, k=19))
+    return password
+
+# create a dictionary to store the website names and generated passwords
+passwords = {}
+
+# define the websites for which to generate passwords
+website_names = ["Facebook", "Twitter", "LinkedIn", "GitHub", "Roblox", "Discord", "Gmail/Google", "TikTok"]
+
+# generate a password for each website
+for website in website_names:
+    password = generate_password()
+    # store the website and generated password in the dictionary
+    passwords[website] = password
+
+# print the generated passwords
+print(passwords)
+
+# write the passwords to a file
+with open("generated_passwords.txt", "w") as f:
+    for website, password in passwords.items():
+        f.write(f"{website}: {password}\n")
+    
